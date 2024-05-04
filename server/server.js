28e5d649cdd161746f23b3a0f3bffd25f0198214
justin <justinlin@ufl.edu>
2024-05-04 07:02:32 -0400

    created options for cors

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -5,11 +6,14 @@ const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
 app.use(cors())
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
