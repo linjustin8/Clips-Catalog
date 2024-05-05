7bc74cbc38edd3ec0585d1fc2dc69cd91930540b
justin <justinlin@ufl.edu>
2024-05-05 08:24:31 -0400

    added unique user id attribute to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
+    },
+    uuid: {
+        type: String,
+        default: uuidv4, 
+        unique: true 
     },
     password: {
         type: String,
