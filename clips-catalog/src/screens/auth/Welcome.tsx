9b794fd0bc021c1e3bf5ca5159db455f34cc2d33
justin <justinlin@ufl.edu>
2024-05-20 13:51:11 -0400

    more work in welcome page

--- clips-catalog/src/screens/auth/Welcome.tsx
+++ clips-catalog/src/screens/auth/Welcome.tsx
@@ -6,17 +6,18 @@ import './Welcome.css'
 function Welcome() {
     
     return  (   
-        <>
-            <div className="">
-                
+        <div className="container">
+            <div className="info">
+                ipsum Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis illo sint suscipit ab quas ullam aliquid mollitia iusto, reprehenderit omnis sit consequatur architecto voluptas dolorem dignissimos, tempora natus deleniti vero?
             </div>
             <div className="bgVideo">
                 <video autoPlay muted loop>
                     <source src="/background_video.mp4" />
                 </video>
             </div>
-        </>
+        </div>
     )   
 }
 
 export default Welcome
+ 
