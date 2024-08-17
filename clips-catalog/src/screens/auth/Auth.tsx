5bd178e0cd908c61cf60e7ab17d1c64d517dfbd6
justin <justinlin@ufl.edu>
2024-08-17 16:48:19 -0400

    created popup and its logic

--- clips-catalog/src/screens/auth/Auth.tsx
+++ clips-catalog/src/screens/auth/Auth.tsx
@@ -3,6 +3,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import validator from "validator";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../../contexts/AuthContext";
 import { InputField } from "../../components/InputField";
 import "./Auth.css";
@@ -16,30 +18,47 @@ const Login: React.FC = () => {
 
   const isValidEmail = validator.isEmail(email);
   const isValidPassword = password.length >= 6;
-  
+
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!allowSubmission) {
       return;
     }
-    
+
     try {
       await login(email, password);
     } catch (err) {
-      if(axios.isAxiosError(err)) {
-        console.log(err.response?.data.message);
-        setError(err.response?.data.message);
+      if (axios.isAxiosError(err)) {
+        const errorMessage = err.response?.data.message
+        setError(errorMessage);
+        console.log(errorMessage);
       }
       setPassword("");
     }
   };
-  
+
   useEffect(() => {
     setAllowSubmission(isValidEmail && isValidPassword);
   }, [email, password]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError("");
+    }, 3000)
+    
+    return () => clearTimeout(timer);
+  }, [error]);
   
   return (
     <div className="page-container">
+      {error ? (
+        <div className="error-popup">
+          <h1>{error}</h1>
+          <button onClick={() => setError("")}>
+            <FontAwesomeIcon className="icon" icon={faXmark} />
+          </button>
+        </div>
+      ) : null}
       <div className="content-container">
         <h1 className="header">Log In</h1>
         <form
@@ -65,7 +84,13 @@ const Login: React.FC = () => {
             type="password"
           />
         </form>
-        <button form="login" className={`submit-user-info ${allowSubmission ? "allow-submit" : ""}`} disabled={!allowSubmission}>
+        <button
+          form="login"
+          className={`submit-user-info ${
+            allowSubmission ? "allow-submit" : ""
+          }`}
+          disabled={!allowSubmission}
+        >
           <p>LOGIN</p>
         </button>
       </div>
@@ -82,25 +107,25 @@ const Signup: React.FC = () => {
 
   const isValidEmail = validator.isEmail(email);
   const isValidPassword = password.length >= 6;
-  
+
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!allowSubmission) {
       return;
     }
-    
+
     try {
-      await signup({username, email, password});
+      await signup({ username, email, password });
     } catch (err) {
       setPassword("");
       console.log(err);
     }
   };
-  
+
   useEffect(() => {
     setAllowSubmission(isValidEmail && isValidPassword);
   }, [email, password]);
-  
+
   return (
     <div className="page-container">
       <div className="content-container">
@@ -135,7 +160,13 @@ const Signup: React.FC = () => {
             type="password"
           />
         </form>
-        <button form="signup" className={`submit-user-info ${allowSubmission ? "allow-submit" : ""}`} disabled={!allowSubmission}>
+        <button
+          form="signup"
+          className={`submit-user-info ${
+            allowSubmission ? "allow-submit" : ""
+          }`}
+          disabled={!allowSubmission}
+        >
           <p>SIGN UP</p>
         </button>
       </div>
